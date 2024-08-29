b2a1c6901890a90f0bdd59e43e0b6a2e700d40ca
emichael <emichael@duchesnay.com>
2024-08-29 17:01:43 -0400

    projects

--- Nuxt/Landing/components/work.vue
+++ Nuxt/Landing/components/work.vue
@@ -19,97 +19,129 @@
                 </div>
             </div>
             <div class="grid grid-cols-1 mt-14 gap-7">
-                <div class="flex flex-wrap lg:flex-nowrap items-stretch border-2 border-gray/20 dark:border-white/[12%]" data-aos="fade-up">
-                    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
-                        <p class="text-warning text-[22px] font-bold">Sight</p>
-                        <a href="javascript:;" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
-                            Your Trusted Financial Companion for Smart Money Management
-                        </a>
-                        <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-                        <div class="flex items-center gap-4 mt-5 md:mt-8">
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/figma-ex.svg" alt="">
-                            </div>
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/sketch-ex.svg" alt="">
-                            </div>
-                        </div>
-                        <div class="inline-block mt-8">
-                            <a href="javascript:;" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
-                                View Project
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
-                        <img src="@/assets/images/work-1.png" class="object-cover w-full h-full mx-auto" alt="">
-                    </div>
-                </div>
-                <div class="flex flex-wrap lg:flex-nowrap items-stretch border-2 border-gray/20 dark:border-white/[12%]" data-aos="fade-up">
-                    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
-                        <p class="text-purple text-[22px] font-bold">Artisan</p>
-                        <a href="javascript:;" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
-                            An app to empower women in their fertility journey.
-                        </a>
-                        <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-                        <div class="flex items-center gap-4 mt-5 md:mt-8">
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/figma-ex.svg" alt="">
-                            </div>
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/sketch-ex.svg" alt="">
-                            </div>
-                        </div>
-                        <div class="inline-block mt-8">
-                            <a href="javascript:;" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
-                                View Project
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
-                        <img src="@/assets/images/work-2.png" class="object-cover w-full h-full mx-auto" alt="">
-                    </div>
-                </div>
-                <div class="flex flex-wrap lg:flex-nowrap items-stretch border-2 border-gray/20 dark:border-white/[12%]" data-aos="fade-up">
-                    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
-                        <p class="text-[#453DB1] text-[22px] font-bold">Poker Queen</p>
-                        <a href="javascript:;" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
-                            Unlock the Mysteries of Fun and Strategy
-                        </a>
-                        <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
-                        <div class="flex items-center gap-4 mt-5 md:mt-8">
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/figma-ex.svg" alt="">
-                            </div>
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/ai-ex.svg" alt="">
-                            </div>
-                            <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
-                                <img src="@/assets/images/ps-ex.svg" alt="">
-                            </div>
-                        </div>
-                        <div class="inline-block mt-8">
-                            <a href="javascript:;" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
-                                View Project
-                                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
-                        <img src="@/assets/images/work-3.png" class="object-cover w-full h-full mx-auto" alt="">
-                    </div>
-                </div>
-                <div class="text-center" data-aos="fade-up">
-                    <a href="javascript:;" class="btn btn-dark">View All Project</a>
-                </div>
-            </div>
+  <div class="flex flex-wrap lg:flex-nowrap items-stretch " data-aos="fade-up">
+
+    <!-- Ludmer Engineering -->
+    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
+      <p class="text-[#4588cb] text-[22px] font-bold">Ludmer Engineering - Quebec</p>
+      <a href="#" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
+        Where contractors go for efficient design
+      </a>
+      <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+      <div class="inline-block mt-8">
+        <a href="https://www.ludmer-engineering.ca/" target="_blank"  rel="noopener noreferrer" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
+          View Project
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
+      <img src="/images/ludmer.webp" class="object-cover w-full h-auto mx-auto" alt="Ludmer Engineering">
+    </div>
+  </div>
+
+  <!-- Duchesnay -->
+  <div class="flex flex-wrap lg:flex-nowrap items-stretch" data-aos="fade-up">
+    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
+      <p class="text-[#72043f] text-[22px] font-bold">Duchesnay - Canada</p>
+      <a href="#" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
+        An app to empower women in their fertility journey.
+      </a>
+      <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+      <!-- <div class="flex items-center gap-4 mt-5 md:mt-8">
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/figma-ex.svg" alt="Figma Icon">
+        </div>
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/sketch-ex.svg" alt="Sketch Icon">
+        </div>
+      </div> -->
+      <div class="inline-block mt-8">
+        <a href="https://duchesnay.com/en" target="_blank"  rel="noopener noreferrer" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
+          View Project
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
+      <img src="/images/duchesnay.webp" class="object-cover w-full h-auto mx-auto" alt="Artisan Project">
+    </div>
+  </div>
+
+  <!-- Analog Pharma -->
+  <div class="flex flex-wrap lg:flex-nowrap items-stretch" data-aos="fade-up">
+    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
+      <p class="text-[#079779] text-[22px] font-bold">Analog Pharma - Canada</p>
+      <a href="#" class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
+        Unlock the Mysteries of Fun and Strategy
+      </a>
+      <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+      <!-- <div class="flex items-center gap-4 mt-5 md:mt-8">
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/figma-ex.svg" alt="Figma Icon">
+        </div>
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/ai-ex.svg" alt="Adobe Illustrator Icon">
+        </div>
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/ps-ex.svg" alt="Photoshop Icon">
+        </div>
+      </div> -->
+      <div class="inline-block mt-8">
+        <a href="https://analogpharma.ca/en" target="_blank"  rel="noopener noreferrer" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
+          View Project
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
+      <img src="/images/analog.webp" class="object-cover w-full h-auto mx-auto" alt="Poker Queen Project">
+    </div>
+  </div>
+<!-- Sefon Surfboards -->
+  <div class="flex flex-wrap lg:flex-nowrap items-stretch" data-aos="fade-up">
+    <div class="order-2 p-10 md:p-12 lg:flex-1 lg:order-1">
+      <p class="text-[#198da4] text-[22px] font-bold">Sefon Surfboards - Peru</p>
+      <a href="#"  class="font-bold text-2xl/normal md:text-3xl/normal lg:text-[40px]/normal inline-block mt-5">
+        Unlock the Mysteries of Fun and Strategy
+      </a>
+      <p class="text-gray max-w-[420px] mt-3 md:mt-5">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+      <!-- <div class="flex items-center gap-4 mt-5 md:mt-8">
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/figma-ex.svg" alt="Figma Icon">
+        </div>
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/ai-ex.svg" alt="Adobe Illustrator Icon">
+        </div>
+        <div class="border-2 border-gray/[14%] h-14 w-14 rounded-full flex items-center justify-center">
+          <img src="@/assets/images/ps-ex.svg" alt="Photoshop Icon">
+        </div>
+      </div> -->
+      <div class="inline-block mt-8">
+        <a href="https://sefon-surfboards.vercel.app/" target="_blank"  rel="noopener noreferrer" class="flex items-center gap-2.5 py-3.5 text-sm font-semibold px-5 border-2 border-dark duration-300 hover:bg-dark hover:text-white dark:border-white/[14%] dark:hover:bg-white dark:hover:text-dark">
+          View Project
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.0035 7.4083L5.41176 16L4 14.5882L12.5917 5.99654H5.01905V4H16V14.981H14.0035V7.4083Z" fill="currentColor" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="xl:max-w-[670px] lg:max-w-[400px] w-full lg:order-2 order-1">
+      <img src="/images/sefon.webp" class="object-cover w-full h-auto mx-auto" alt="Sefon Surfboards website">
+    </div>
+  </div>
+
+  <div class="text-center" data-aos="fade-up">
+    <a href="#" class="btn btn-dark">View All Projects</a>
+  </div>
+</div>
+
         </div>
     </section>
     <!-- End Work -->
